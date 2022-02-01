00100f8383fcaef2906544ee9ef22174d90220af
TTNguyen <nttriet9a4@gmail.com>
2022-02-01 14:28:01 +0700

    add log for insert tx to pools

--- chain/client/src/client_actor.rs
+++ chain/client/src/client_actor.rs
@@ -963,7 +963,7 @@ impl ClientActor {
     /// Process all blocks that were accepted by calling other relevant services.
     fn process_accepted_blocks(&mut self, accepted_blocks: Vec<AcceptedBlock>) {
         for accepted_block in accepted_blocks {
-            let accepted_block_clone = accepted_block.clone();
+// let accepted_block_clone = accepted_block.clone();
             self.client.on_block_accepted(
                 accepted_block.hash,
                 accepted_block.status,
